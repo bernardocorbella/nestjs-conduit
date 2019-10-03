c5ade01bdec8efb560f7032dc9fa9f3b45c02139
Bernardo Corbella <corbellabernardo@gmail.com>
2019-10-03 22:13:22 +0200

    adds base db config and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJS Conduit Example App')
+    .setDescription('The Conduit API description')
+    .setVersion('1.0')
+    .setBasePath('api')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
