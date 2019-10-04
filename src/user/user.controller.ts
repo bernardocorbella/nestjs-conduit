297b7cc508b9faf5cf37b9f9bc35c36c25503e3e
Bernardo Corbella <corbellabernardo@gmail.com>
2019-10-04 19:04:04 +0200

    adds custom validation pipe

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ValidationPipe } from '../shared/validation.pipe';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UsePipes(new ValidationPipe())
   @Post('users')
   async create(@Body('user') createUserData: CreateUserDto) {
     return this.userService.create(createUserData);
